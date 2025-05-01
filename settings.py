97b605a39d09df609790df16ab0a8b915bd7bc8d
Aniket H Shinde <161929446+aniketshinde8483@users.noreply.github.com>
2025-05-01 19:29:53 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,5 +7,5 @@ WEBCAM = "Webcam"
 
 SOURCES_LIST = ["Image", "Video", "Webcam"]
 
-MODEL_PATH = "weights/newbest.pt"  # Your YOLOv8 model this is localy traind best.pt
+MODEL_PATH = "weights/ppe.pt"  # Your YOLOv8 model this is localy traind best.pt
 #MODEL_PATH = "weights\\bestsafety.pt"  # Your YOLOv8 model this is localy traind best.pt 
